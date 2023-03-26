386524e6854c732847750ca328b557d899cff830
chellick <matsupus@gmail.com>
2023-03-26 23:17:20 +0300

    /help added upd.

--- scripts/bot.py
+++ scripts/bot.py
@@ -2,6 +2,8 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import telegram
 import aiogram as ai
 import os
+import asyncio as io
+
 
 
 
@@ -10,7 +12,7 @@ file_p = os.path.join('C:/Users/Matvey/OneDrive/Рабочий стол/Token bo
 with open(file_p, 'r') as f:
     TOKEN = f.read()
 
-print(TOKEN)
+
 
 
 bot = ai.Bot(token=TOKEN)
@@ -19,9 +21,29 @@ disp = ai.Dispatcher(bot, storage)
 
 @disp.message_handler(commands="start")
 async def start(message: ai.types.Message):
+    user_id = message.from_user.id
     await message.answer("Привет! Я Test Habr Bot!")
+
+
+
+
+
+@disp.message_handler(commands="help")
+async def help(message: ai.types.Message):
+    text = ''
+    commands = [
+        "/start - начать работу",
+        "/help - список всех команд"
+    ]
+    help_message = "Список всех доступных команд:\n\n"
+    help_message += "\n".join(commands)
+    await message.answer(help_message)
 
 
 
 if __name__ == '__main__':
     ai.utils.executor.start_polling(disp, skip_updates=True)
+
+
+
+
