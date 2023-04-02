ace695335fdd260895d5bca7180b81bc4ed36ec2
chellick <matsupus@gmail.com>
2023-04-02 23:38:02 +0300

    pars working upd.

--- scripts/parc.py
+++ scripts/parc.py
@@ -1,22 +1,18 @@
 import requests
 from bs4 import BeautifulSoup as BS
 
-r = requests.get("https://habr.com/ru/hubs/")
-html = BS(r.content, "html.parser")
-arr = html.find_all("a", class_="tm-hub__title")
-
 
 def get_theme(array):
-    arr = []
-    for el in array:
-        arr.append(el.span.text)
+    themes = []
+    for el in array:     
+        themes.append((el.span.text, "https://habr.com" + el["href"]))
 
-    return arr
+    return themes
 
 
-    
-# for el in html.select("#app > div.tm-layout__wrapper > div.tm-layout > main > div > div > div > div.tm-page__main.tm-page__main_has-sidebar > div > div:nth-child(3) > div.tm-hubs-list"):
-#     for i in range(1, 10):
+for i in range(1, 12):
+    r = requests.get(f"https://habr.com/ru/hubs/page{i}")
+    html = BS(r.content, "html.parser")
+    arr = html.find_all("a", class_="tm-hub__title")
+    print(get_theme(arr))
 
-#         title = el.select(f"#app > div.tm-layout__wrapper > div.tm-layout > main > div > div > div > div.tm-page__main.tm-page__main_has-sidebar > div > div:nth-child(3) > div.tm-hubs-list > div:nth-child({i}) > div > div.tm-hub > div > div:nth-child(1) > a > span")
-#         print(title)
